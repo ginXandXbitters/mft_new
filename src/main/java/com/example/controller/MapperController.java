b827a2f692a5e48a52f070bb43afe95ac4625c96
1011ozw <ziwen_ou@126.com>
2019-06-24 14:09:15 +0800

    增加用户主页index_user.html

--- src/main/java/com/example/controller/MapperController.java
+++ src/main/java/com/example/controller/MapperController.java
@@ -5,6 +5,7 @@ import com.example.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -12,22 +13,18 @@ import java.util.List;
 @Controller
 public class MapperController {
     @Autowired
-    private UserMapper userMapper;
-    @RequestMapping("/rigister.html")
-    @ResponseBody
-    public List<User> queryUser(){
-        List<User> users = userMapper.queryUserList();
-        return users;
-    }
-@RequestMapping("/")
-public String test(){
+
+    @RequestMapping("/")
+    public String homepage(){
         return "index";
-}
-    @RequestMapping("/login.html")
-    //@ResponseBody
-    public String queryUsername(){
-        String myname = userMapper.queryUsername("u000001");
-        System.out.println(myname);
-        return "login";
     }
+
+
+    @RequestMapping("/index.html")
+    public String homepage_index(){
+        return "index";
+    }
+
+
+
 }
