39adf94275e5322767e1f66cb6831ee51703e755
ginXandXbitters <ocean98@126.com>
2019-06-24 20:15:15 +0800

    注册功能完成

--- src/main/java/com/example/controller/RegisterController.java
+++ src/main/java/com/example/controller/RegisterController.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import com.example.domain.Store;
+import com.example.domain.Suppliers;
+import com.example.domain.User;
 import com.example.mapper.RegisterMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,19 +20,51 @@ public RegisterMapper registerMapper;
     }
     @RequestMapping("/login_afterregister.html")
     public String userInsert(HttpServletRequest request){
+
+
         String userID=request.getParameter("telephone");
         String userPw=request.getParameter("password");
         String userName=request.getParameter("name");
         String userTel=request.getParameter("telephone");
         String userMail=request.getParameter("email");
-        String province=request.getParameter("");
-        String city=request.getParameter("name");
-        String county=request.getParameter("name");
-        String site=request.getParameter("name");
+        String province=request.getParameter("company");
+        String city=request.getParameter("address_1");
+        String county=request.getParameter("address_2");
+        String site=request.getParameter("postcode");
         int status=0;
 
+        User new_user=new User();
+        new_user.setUserID(userID);               //用户注册所需信息
+        new_user.setUserPw(userPw);
+        new_user.setUserName(userName);
+        new_user.setUserTel(userTel);
+        new_user.setUserMail(userMail);
+        new_user.setProvince(province);
+        new_user.setCity(city);
+        new_user.setCounty(county);
+        new_user.setSite(site);
+        new_user.setStatus(status);
 
 
+        Store new_store=new Store();
+        new_store.setStoreID(userID);               //商家注册所需信息
+        new_store.setStorePW(userPw);
+        new_store.setStoreName(userName);
+        new_store.setSupplierID("sup02");
+        new_store.setStatus(1);
+
+        Suppliers new_supplier=new Suppliers();
+        new_supplier .setSupplierID(userID);              //供货商注册所需信息
+        new_supplier.setSupplierName(userName);
+        new_supplier.setSupplierPW(userPw);
+        new_supplier.setStoreID("s01");
+        new_supplier.setStatus(2);
+
+String role=request.getParameter("newsletter");
+//System.out.println(role);
+        if(role.equals("0")){ registerMapper.insert_newuser(new_user);}
+        if(role.equals("1")){ registerMapper.insert_newstore(new_store);}
+        if(role.equals("2")){ registerMapper.insert_newsupplier(new_supplier);}
         return "login";
     }
 }
--- src/main/java/com/example/mapper/RegisterMapper.java
+++ src/main/java/com/example/mapper/RegisterMapper.java
@@ -1,16 +1,19 @@
 package com.example.mapper;
 
+import com.example.domain.Store;
+import com.example.domain.Suppliers;
+import com.example.domain.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface RegisterMapper {
-    @Insert("insert into user(userID,userPw,userName,userTel,userMail,province,city,county,site,status) values(#{id},#{pw},#{name},#{tel},#{mail},#{province},#{city},#{county},#{site},#{status})")
- public int insert_user(String id,String pw,String name,String tel,String mail,String province,String city,String county,String status);
+    @Insert("insert into user(userID,userPw,userName,userTel,userMail,province,city,county,site,status) values(#{userID},#{userPw},#{userName},#{userTel},#{userMail},#{province},#{city},#{county},#{site},#{status})")
+    public void insert_newuser(User user);
 
-//    @Insert("insert into store(storeID,storeName,supplierID) values(#{id},#{pw},#{name},#{tel},#{mail})")
-//    public int insert_store(String id,String name,String supplier);
-//
-//    @Insert("insert into suppliers(userID,userPw,userName,userTel,userMail) values(#{id},#{pw},#{name},#{tel},#{mail})")
-//    public int insert_supplier(String id,String pw,String name,String tel,String mail);
+    @Insert("insert into store(storeID,storePW,storeName,supplierID,status) values(#{storeID},#{storePW},#{storeName},#{supplierID},#{status})")
+    public void insert_newstore(Store store);
+
+    @Insert("insert into suppliers(supplierID,supplierPW,supplierName,storeID,status) values(#{supplierID},#{supplierPW},#{supplierName},#{storeID},#{status})")
+    public void insert_newsupplier(Suppliers suppliers);
 }
